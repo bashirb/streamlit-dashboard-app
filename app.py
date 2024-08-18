5984b07c852e9ee9c6acf055c75fed8fecc3c256
bashirb <26bash@gmail.com>
2024-08-18 13:13:52 +0100

    page layout setup

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ import pandas as pd
 from dotenv import load_dotenv
 import altair as alt
 
+
+# page layout setup
+st.set_page_config(
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+st.write("## Financial Market Analysis Dashboard")
+st.divider()
+
